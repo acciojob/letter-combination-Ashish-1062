fb119a0929ffb872c83fa1892fc44934fb56f5f8
Ashish-1062 <83691659+Ashish-1062@users.noreply.github.com>
2023-06-21 14:53:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
 function letterCombinations(input_digit) {
-  //Complete the function
+  // Mapping of digit to letters
+  const digitMap = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
+    '0': '0',
+    '1': '1'
+  };
+
+  // Base case: if the input is an empty string, return an empty array
+  if (input_digit === '') {
+    return [];
+  }
+
+  // Recursive case: generate letter combinations
+  const combinations = [];
+
+  // Recursive function to generate combinations
+  function generateCombinations(currentIndex, currentCombination) {
+    if (currentIndex === input_digit.length) {
+      combinations.push(currentCombination);
+      return;
+    }
+
+    const currentDigit = input_digit[currentIndex];
+    const letters = digitMap[currentDigit];
+
+    for (let i = 0; i < letters.length; i++) {
+      const letter = letters[i];
+      generateCombinations(currentIndex + 1, currentCombination + letter);
+    }
+  }
+
+  generateCombinations(0, '');
+
+  return combinations;
 }
 
 module.exports = letterCombinations;
